c25f99a3b1c7c3e2f8f27a9343ec31dc50de4c35
carlosensantos <carlosensantos@gmail.com>
2024-06-19 14:27:36 -0300

    implementação do detalhar Autor

--- src/controladores/AutorControlador.ts
+++ src/controladores/AutorControlador.ts
@@ -5,4 +5,21 @@ export default class AutorControlador{
     listar(req: Request, res: Response){
         return res.status(200).json(autores)
     }
+
+    detalhar(req: Request, res: Response){
+        const { id } = req.params
+
+        const autor = autores.find((elemento) => {
+            return elemento.id === id
+        })
+
+        if(!autor){
+            return res.status(404).json({
+                mensagem: 'Autor não encontrado'
+            })
+        }
+
+        return res.status(200).json(autor)
+
+    }
 }
