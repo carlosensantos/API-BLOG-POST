0a04994d158cf42a2e6491f1868acf35619062e0
carlosensantos <carlosensantos@gmail.com>
2024-06-20 10:09:00 -0300

    implementação do listar postagens

--- src/bancoDeDados.ts
+++ src/bancoDeDados.ts
@@ -2,4 +2,13 @@ import Autor from "./modelos/Autor";
 import Post from "./modelos/Post";
 
 export const autores: Autor[] = []
-export const posts: Post[] = []
+export const posts: Post[] = [
+    new Post ({
+        titulo: 'meu primeiro post',
+        descricao: 'descrição do post',
+        autor: new Autor ({
+            nome: 'carlos',
+            idade: 33
+        })
+    })
+]
